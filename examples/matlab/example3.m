93c52f830909b2539d8dd6f932336d91bef24bc3
kadu0001 <a.a.kadu@uu.nl>
2018-03-31 02:29:53 +0200

    changes

--- examples/matlab/example3.m
+++ examples/matlab/example3.m
@@ -36,11 +36,11 @@
     out.x = in_image(rid,cid:cidm);
     out.y = out_image(rid,cid:cidm);
     out.z = out_image2(rid,cid:cidm);
-    figure();plot(out.x,'Color',[0.66 0.66 0.66],'LineWidth',1.5);hold on;plot(out.y,'LineWidth',3);
+    figure(1);plot(out.x,'Color',[0.66 0.66 0.66],'LineWidth',1.5);hold on;plot(out.y,'LineWidth',3);
     plot(out.z,':','LineWidth',3);hold off;axis off;
     pbaspect([3 1 1])
     lgd = legend('True','Mumford-Shah','TV','Location','southoutside','Orientation','horizontal');
-    lgd.FontSize = 20;
+    lgd.FontSize = 25;
     lgd.EdgeColor = 'w';
     saveas(gcf,'1dex','epsc')
     saveas(gcf,'1dex','fig')
